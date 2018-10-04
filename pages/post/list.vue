19fbb647e9a4f98a5ca41ab8b74062d1c6d956d3
Haider Malik <haidermalik504@gmail.com>
2018-10-04 14:29:13 +0500

    render list

--- pages/post/list.vue
+++ pages/post/list.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>Post List</h1>
+  <div>
+    <h1>Post List</h1>
+    <ol>
+      <li v-for="post in posts" :key="post.id">{{post.title}} </li>
+    </ol>
+  </div>
 </template>
 
 <script>
+import {mapState} from 'vuex';
 export default {
-name: 'PostList'
+name: 'PostList',
+computed: {
+  ...mapState({
+    posts: state => state.post.posts
+  })
+}
 }
 </script>
 
--- store/modules/post.js
+++ store/modules/post.js
@@ -1,5 +1,24 @@
 const state = () => ({
-  posts: []
+  posts: [
+    {
+      id: '1',
+      title: 'Learn NodeJs',
+      description:
+        'he industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. I'
+    },
+    {
+      id: '2',
+      title: 'Learn NuxtJs',
+      description:
+        'he industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. I'
+    },
+    {
+      id: '3',
+      title: 'Learn VueJs',
+      description:
+        'he industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. I'
+    }
+  ]
 })
 const getters = {}
 const actions = {}
