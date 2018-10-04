bd600346bf7b5cd774c70a374fd3296ef6a0b1f9
Haider Malik <haidermalik504@gmail.com>
2018-10-04 16:15:35 +0500

    form validations in element ui

--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
     <el-main>
-      <el-form ref="form" :model="form" label-width="120px">
-        <el-form-item label="Title">
+      <el-form ref="form" :model="form" label-width="120px" :rules="rules">
+        <el-form-item label="Title" prop="title">
           <el-input v-model="form.title"></el-input>
         </el-form-item>
-        <el-form-item label="Description">
+        <el-form-item label="Description" prop="description">
           <el-input v-model="form.description" type="textarea"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">Create</el-button>
-          <el-button>Cancel</el-button>
+          <el-button type="primary" @click="onSubmit('form')">Create</el-button>
+          <el-button @click="resetForm('form')">Cancel</el-button>
         </el-form-item>
       </el-form>
     </el-main>
@@ -25,13 +25,27 @@ data(){
     form: {
     title: '',
     description: ''
+    },
+    rules: {
+      title:  { required: true, message: 'Please provide post title', trigger: 'blur' },
+      description:  { required: true, message: 'Please provide post description', trigger: 'blur' },
     }
   }
 },
 methods:{
-  onSubmit(){
-    alert('Form submitted')
-  }
+  onSubmit(formName){
+      this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+  },
+  resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
 }
 }
 </script>
